994f4a070eca9fc2fc840995f0ec125b6be6e803
MykRud <rydukmisha777@gmail.com>
2021-11-17 15:00:24 +0200

    Commit

---
+++ src/com/company/Task_3/SortingTest.java
@@ -0,0 +1,39 @@
+package com.company.Task_3;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortingTest {
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Before CalculatorTest.class");
+    }
+
+    @AfterClass
+    public  static void afterClass() {
+        System.out.println("After CalculatorTest.class");
+    }
+
+    @Test
+    public void sortingAscending() throws IOException {
+        assertTrue(Sorting.IsSorted(new int[]{1, 2, 6, 7}, SortOrder.Ascending));
+    }
+
+    @Test
+    public void sortingDescending() throws IOException {
+        assertTrue(Sorting.IsSorted(new int[]{9, 7, 4, 1}, SortOrder.Descending));
+    }
+
+    @Test(expected = IOException.class)
+    public void isSorted() throws IOException {
+        Sorting.IsSorted(new int[]{3, 2, 6, 7}, SortOrder.Ascending);
+        Sorting.IsSorted(new int[]{3, 2, 6, 7}, SortOrder.Descending);
+    }
+
+}
